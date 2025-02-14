7d1f2ba66309d16520c8acfeb4601119f3789c04
Royal-design <royaldesign1999@gmail.com>
2025-02-14 09:39:24 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-[#02191D] max-md:px-[20px]">
+    <div className="w-full [@media(min-width:786px)_and_(max-width:1000px)]:h-screen  bg-[#02191D] bg-[radial-gradient(52.52%_32.71%_at_50%_97.66%,rgba(36,160,181,0.2)_0%,rgba(36,160,181,0)_100%)] max-md:px-[20px]">
       <Navbar />
       <MultiStepForm />
     </div>
--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -7,7 +7,7 @@ export const MultiStepForm = () => {
   const step = useAppSelector((state) => state.form.step);
 
   return (
-    <div className="flex w-full  flex-col items-center h-auto  pt-[46px] pb-[112px] max-md:pt-[20px] max-md:h-auto bg-[#02191D] bg-[radial-gradient(52.52%_32.71%_at_50%_97.66%,rgba(36,160,181,0.2)_0%,rgba(36,160,181,0)_100%)] ">
+    <div className="flex w-full justify-center  items-center   pt-[46px] pb-[112px] max-md:pt-[20px] max-md:h-auto  ">
       {step === 1 && <Form1 />}
       {step === 2 && <Form2 />}
       {step === 3 && <Ticket />}
